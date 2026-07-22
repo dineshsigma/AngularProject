2b1806867855646786786d4f637e2e6fd526a75d
Gundelugunta Dinesh <Gundelugunta.D@coforge.com>
2026-07-22 15:51:57 +0530

    dark mode and light mode Implemention

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
-  constructor(private router: Router) {
-}
+export class Navbar implements OnInit {
+  constructor(private router: Router) { }
+  isDarkMode = false;
+
+
+  ngOnInit(): void {
+
+    const savedTheme =
+      localStorage.getItem('theme') || 'light';
+
+    this.isDarkMode = savedTheme === 'dark';
+
+    document.body.setAttribute(
+      'data-theme',
+      savedTheme
+    );
+  }
+
+
+  toggleTheme(): void {
+
+    this.isDarkMode = !this.isDarkMode;
+
+    const theme =
+      this.isDarkMode ? 'dark' : 'light';
+
+    document.body.setAttribute(
+      'data-theme',
+      theme
+    );
+
+    localStorage.setItem(
+      'theme',
+      theme
+    );
+  }
+
+
 
   logout() {
     this.router.navigate(['/login']);
