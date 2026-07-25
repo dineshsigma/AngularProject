8b209b59d6fcccd6b09cae7d99f606cb9827a467
Gundelugunta Dinesh <Gundelugunta.D@coforge.com>
2026-07-25 09:59:58 +0530

    Cleaning Code

--- src/app/shared/dynamic-table/dynamic-table.ts
+++ src/app/shared/dynamic-table/dynamic-table.ts
@@ -14,37 +14,24 @@ import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-dynamic-table',
-  imports: [CommonModule,FormsModule,TooltipModule],
-   standalone: true,
+  imports: [CommonModule, FormsModule, TooltipModule],
+  standalone: true,
   templateUrl: './dynamic-table.html',
   styleUrl: './dynamic-table.css',
 })
 export class DynamicTable {
-
   @Input() title = '';
-
   @Input() columns: TableColumn[] = [];
-
   @Input() data: any[] = [];
-
   @Input() showAddButton = false;
-
   @Input() addButtonText = 'Add';
-
   @Input() redirectUrl = '';
-
   @Input() totalRecords = 0;
-
   @Input() currentPage = 1;
-
   @Input() pageSize = 10;
-
   @Input() loading = false;
-
   @Input() showSearch = false;
-
   @Input() searchFields: string[] = [];
-
   @Input() pageSizeOptions = [
     10,
     20,
@@ -52,48 +39,16 @@ export class DynamicTable {
     40,
     50
   ];
-
   @Output() edit = new EventEmitter<any>();
-
   @Output() delete = new EventEmitter<any>();
-
   @Output() pageChange = new EventEmitter<number>();
-
   @Output() pageSizeChange = new EventEmitter<number>();
-
   @Output() search = new EventEmitter<string>();
 
   showDeleteModal = false;
-
   selectedRow: any = null;
-
   searchText = '';
-
-
-   ngOnChanges(
-    changes: SimpleChanges
-  ): void {
-
-    console.log(
-      'Dynamic Table Changes',
-      changes
-    );
-
-    console.log(
-      'Total Records',
-      this.totalRecords
-    );
-  }
-
   constructor(private router: Router) { }
-
-  // ngOnChanges(changes: SimpleChanges): void {
-
-  //   if (changes['totalRecords']) {
-  //     // this.generatePageSizeOptions();
-  //   }
-  // }
-
   addRecord(): void {
     if (this.redirectUrl) {
       this.router.navigate([this.redirectUrl]);
@@ -105,7 +60,6 @@ export class DynamicTable {
   }
 
   openDeleteModal(row: any): void {
-    console.log("row",row);
     this.selectedRow = row;
     this.showDeleteModal = true;
   }
@@ -121,7 +75,6 @@ export class DynamicTable {
   }
 
   get totalPages(): number {
-
     return this.totalRecords > 0
       ? Math.ceil(
         this.totalRecords /
@@ -131,7 +84,6 @@ export class DynamicTable {
   }
 
   previousPage(): void {
-
     if (
       this.currentPage > 1
     ) {
@@ -143,7 +95,6 @@ export class DynamicTable {
   }
 
   nextPage(): void {
-
     if (
       this.currentPage <
       this.totalPages
@@ -156,44 +107,36 @@ export class DynamicTable {
   }
 
   changePageSize(
-  event: Event
-): void {
+    event: Event
+  ): void {
 
-  const pageSize = Number(
-    (event.target as HTMLSelectElement)
-      .value
-  );
+    const pageSize = Number(
+      (event.target as HTMLSelectElement)
+        .value
+    );
 
-  this.pageSizeChange.emit(
-    pageSize
-  );
-}
+    this.pageSizeChange.emit(
+      pageSize
+    );
+  }
+  applySearch(): void {
+    let searchValue = this.searchText;
+    this.search.emit(searchValue)
+  }
+  getBadgeStyle(status: string): string {
+    const statusMap: { [key: string]: string } = {
+      'In Stock': 'success',
+      'Low Stock': 'warning',
+      'Out Of Stock': 'danger',
+      'Active': 'success',
+      'Inactive': 'danger',
+      'Pending': 'warning',
+      'Approved': 'success',
+      'Rejected': 'danger',
+      'Processing': 'info',
+      'Completed': 'success'
+    };
 
-
-applySearch():void{
-  console.log(this.searchText,"searchText");
-  let searchValue  =  this.searchText;
-  this.search.emit(searchValue)
-}
-
-
-getBadgeStyle(status: string): string {
-  const statusMap: { [key: string]: string } = {
-    'In Stock': 'success',
-    'Low Stock': 'warning',
-    'Out Of Stock': 'danger',
-    'Active': 'success',
-    'Inactive': 'danger',
-    'Pending': 'warning',
-    'Approved': 'success',
-    'Rejected': 'danger',
-    'Processing': 'info',
-    'Completed': 'success'
-  };
-
-  return statusMap[status] || 'default';
-}
-
-
-
+    return statusMap[status] || 'default';
+  }
 }
